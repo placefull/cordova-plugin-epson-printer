3df1dc0b8688e5744ca97d6abe3f9ca259dcf97e
Jordan Matthews <jordan@placefull.com>
2016-11-16 11:01:25 -0800

    Added new methods

--- src/ios/EpsonPrinter.h
+++ src/ios/EpsonPrinter.h
@@ -4,5 +4,47 @@
 @interface EpsonPrinter : CDVPlugin
 
 - (void) connect:(CDVInvokedUrlCommand*)command;
+- (void) createBuilder:(CDVInvokedUrlCommand *)command;
+- (void) removeBuilder:(CDVInvokedUrlCommand *)command;
+- (void) addTextLang:(CDVInvokedUrlCommand *)command;
+- (void) addTextAlign:(CDVInvokedUrlCommand *)command;
+- (void) addText:(CDVInvokedUrlCommand *)command;
+- (void) addTextSmooth:(CDVInvokedUrlCommand *)command;
+- (void) addFeedLine:(CDVInvokedUrlCommand *)command;
+- (void) addTextSize:(CDVInvokedUrlCommand *)command; //(long)width Height:(long)height;
+- (void) addTextStyle:(CDVInvokedUrlCommand *)command; //(int)reverse Ul:(int)ul Em:(int)em Color:(int)color;
+- (void) addTextFont:(CDVInvokedUrlCommand *)command; //(int)font;
+- (void) addCut:(CDVInvokedUrlCommand *)command; //(int)type;
+- (void) sendToPrinter:(CDVInvokedUrlCommand *)command;
 
+/*
+- (int) addTextLineSpace:(long)linespc;
+- (int) addTextRotate:(int)rotate;
+- (int) addTextDouble:(int)dw Dh:(int)dh;
+- (int) addTextPosition:(long)x;
+- (int) addFeedUnit:(long)unit;
+- (int) addImage:(UIImage *)data X:(long)x Y:(long)y Width:(long)width Height:(long)height Color:(int)color;
+- (int) addImage:(UIImage *)data X:(long)x Y:(long)y Width:(long)width Height:(long)height Color:(int)color Mode:(int)mode Halftone:(int)halftone Brightness:(double)brightness;
+- (int) addImage:(UIImage *)data X:(long)x Y:(long)y Width:(long)width Height:(long)height Color:(int)color Mode:(int)mode Halftone:(int)halftone Brightness:(double)brightness Compress:(int)compress;
+- (int) addLogo:(long)key1 Key2:(long)key2;
+- (int) addBarcode:(NSString *)data Type:(int)type Hri:(int)hri Font:(int)font Width:(long)width Height:(long)height;
+- (int) addSymbol:(NSString *)data Type:(int)type Level:(int)level Width:(long)width Height:(long)height Size:(long)size;
+- (int) addHLine:(long)x1 X2:(long)x2 Style:(int)style;
+- (int) addVLineBegin:(long)x Style:(int)style;
+- (int) addVLineEnd:(long)x Style:(int)style;
+- (int) addPageBegin;
+- (int) addPageEnd;
+- (int) addPageArea:(long)x Y:(long)y Width:(long)width Height:(long)height;
+- (int) addPageDirection:(int)dir;
+- (int) addPagePosition:(long)x Y:(long)y;
+- (int) addPageLine:(long)x1 Y1:(long)y1 X2:(long)x2 Y2:(long)y2 Style:(int)style;
+- (int) addPageRectangle:(long)x1 Y1:(long)y1 X2:(long)x2 Y2:(long)y2 Style:(int)style;
+- (int) addCut:(int)type;
+- (int) addPulse:(int)drawer Time:(int)time;
+- (int) addSound:(int)pattern Repeat:(long)repeat;
+- (int) addSound:(int)pattern Repeat:(long)repeat Cycle:(long)cycle;
+- (int) addCommand:(NSData *)data;
+- (int) addFeedPosition:(int)position;
+- (int) addLayout:(int)type Width:(long)width Height:(long)height MarginTop:(long)marginTop MarginBottom:(long)marginBottom OffsetCut:(long)offsetCut OffsetLabel:(long)offsetLabel;
+*/
 @end
--- src/ios/EpsonPrinter.m
+++ src/ios/EpsonPrinter.m
@@ -8,11 +8,236 @@
 
 #import "EpsonPrinter.h"
 
+#define SEND_TIMEOUT    10 * 1000
+
 @implementation EpsonPrinter
 
+EposPrint* printer;
+EposBuilder *builder;
+int printerType;
+
+- (int)getBuilderAlign:(int)align {
+    switch(align){
+        case 1:
+            return EPOS_OC_ALIGN_CENTER;
+        case 2:
+            return EPOS_OC_ALIGN_RIGHT;
+        case 0:
+        default:
+            return EPOS_OC_ALIGN_LEFT;
+    }
+}
+
+- (int)getBuilderLanguage:(int)lang {
+    switch(lang){
+        case 1:
+            return EPOS_OC_LANG_JA;
+        case 2:
+            return EPOS_OC_LANG_ZH_CN;
+        case 3:
+            return EPOS_OC_LANG_ZH_TW;
+        case 4:
+            return EPOS_OC_LANG_KO;
+        case 5:
+            return EPOS_OC_LANG_TH;
+        case 6:
+            return EPOS_OC_LANG_VI;
+        case 0:
+        default:
+            return EPOS_OC_LANG_EN;
+    }
+}
+
+- (int)getBuilderFont:(int)font {
+    switch(font){
+        case 1:
+            return EPOS_OC_FONT_B;
+        case 2:
+            return EPOS_OC_FONT_C;
+        case 3:
+            return EPOS_OC_FONT_D;
+        case 4:
+            return EPOS_OC_FONT_E;
+        case 0:
+        default:
+            return EPOS_OC_FONT_A;
+    }
+}
+
+- (int)getBuilderType:(int)cut {
+    switch(cut){
+        case 1:
+            return EPOS_OC_CUT_FEED;
+        case 0:
+        default:
+            return EPOS_OC_CUT_NO_FEED;
+    }
+}
+
 - (void)connect:(CDVInvokedUrlCommand *)command {
-    CDVPluginResult* result;
-    [self.commandDelegate sendPluginResult:result callbackId:[command callbackId]];
+    [self.commandDelegate runInBackground:^{
+        CDVPluginResult* plug;
+        
+        //get open parameter
+        NSString* ipAddress = [command.arguments objectAtIndex:0];
+        if(ipAddress == nil || ipAddress.length == 0){
+            plug = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"Arg was null"];
+        }
+        
+        if (!printer) {
+            printer = [[EposPrint alloc] init];
+            printerType = EPOS_OC_DEVTYPE_TCP;
+            if(printer == nil){
+                plug = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"Could not initialize"];
+            }
+        }
+        int result = [printer openPrinter:printerType DeviceName:ipAddress];
+        if(result != EPOS_OC_SUCCESS) {
+            plug = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"Could open printer at that port"];
+        } else {
+            plug = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
+        }
+        [self.commandDelegate sendPluginResult:plug callbackId:[command callbackId]];
+    }];
+}
+
+-(void)createBuilder:(CDVInvokedUrlCommand *)command {
+    [self.commandDelegate runInBackground:^{
+        CDVPluginResult* plug;
+        if (!builder) {
+            builder = [[EposBuilder alloc] initWithPrinterModel:[command.arguments objectAtIndex:0] Lang:EPOS_OC_MODEL_ANK];
+            if(builder == nil){
+                plug = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"Could not initialize"];
+            }
+            plug = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
+        }
+        [self.commandDelegate sendPluginResult:plug callbackId:[command callbackId]];
+    }];
+    
+}
+
+- (void) removeBuilder:(CDVInvokedUrlCommand *)command {
+    CDVPluginResult* plug = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
+    [builder clearCommandBuffer];
+    [self.commandDelegate sendPluginResult:plug callbackId:[command callbackId]];
+    
+}
+
+- (void)addText:(CDVInvokedUrlCommand *)command {
+    CDVPluginResult* plug;
+    int result = [builder addText:[command.arguments objectAtIndex:0]];
+    if(result != EPOS_OC_SUCCESS){
+        plug = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"Could not add text"];
+    } else {
+        plug = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
+    }
+    [self.commandDelegate sendPluginResult:plug callbackId:[command callbackId]];
+}
+
+- (void)addTextAlign:(CDVInvokedUrlCommand *)command {
+    CDVPluginResult* plug;
+    int result = [builder addTextAlign:[self getBuilderAlign:(int)[command.arguments objectAtIndex:0]]];
+    if(result != EPOS_OC_SUCCESS){
+        plug = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"Could not align text"];
+    } else {
+        plug = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
+    }
+    [self.commandDelegate sendPluginResult:plug callbackId:[command callbackId]];
+}
+
+- (void)addTextLang:(CDVInvokedUrlCommand *)command {
+    CDVPluginResult* plug;
+    int result = [builder addTextLang:[self getBuilderLanguage:(int)[command.arguments objectAtIndex:0]]];
+    if(result != EPOS_OC_SUCCESS){
+        plug = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"Could not set language"];
+    } else {
+        plug = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
+    }
+    [self.commandDelegate sendPluginResult:plug callbackId:[command callbackId]];
+}
+
+- (void)addTextSmooth:(CDVInvokedUrlCommand *)command {
+    CDVPluginResult* plug;
+    int result = [builder addTextSmooth:(int)[command.arguments objectAtIndex:0]];
+    if(result != EPOS_OC_SUCCESS){
+        plug = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"Could not set smooth text"];
+    } else {
+        plug = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
+    }
+    [self.commandDelegate sendPluginResult:plug callbackId:[command callbackId]];
+}
+
+- (void)addFeedLine:(CDVInvokedUrlCommand *)command {
+    CDVPluginResult* plug;
+    int result = [builder addFeedLine:(long)[command.arguments objectAtIndex:0]];
+    if(result != EPOS_OC_SUCCESS){
+        plug = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"Could not add feed line"];
+    } else {
+        plug = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
+    }
+    [self.commandDelegate sendPluginResult:plug callbackId:[command callbackId]];
+}
+
+//(long)width Height:(long)height;
+- (void) addTextSize:(CDVInvokedUrlCommand *)command {
+    CDVPluginResult* plug;
+    int result = [builder addTextSize:(long)[command.arguments objectAtIndex:0] Height:(long)[command.arguments objectAtIndex:1]];
+    if(result != EPOS_OC_SUCCESS){
+        plug = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"Could not set text size"];
+    } else {
+        plug = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
+    }
+    [self.commandDelegate sendPluginResult:plug callbackId:[command callbackId]];
+}
+
+//(int)reverse Ul:(int)ul Em:(int)em Color:(int)color;
+- (void) addTextStyle:(CDVInvokedUrlCommand *)command {
+    CDVPluginResult* plug;
+    int result = [builder addTextStyle:(int)[command.arguments objectAtIndex:0] Ul:(int)[command.arguments objectAtIndex:1] Em:(int)[command.arguments objectAtIndex:2] Color:(int)[command.arguments objectAtIndex:3]];
+    if(result != EPOS_OC_SUCCESS){
+        plug = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"Could not set text style"];
+    } else {
+        plug = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
+    }
+    [self.commandDelegate sendPluginResult:plug callbackId:[command callbackId]];
+}
+
+//(int)font;
+- (void) addTextFont:(CDVInvokedUrlCommand *)command {
+    CDVPluginResult* plug;
+    int result = [builder addTextFont:[self getBuilderFont:(int)[command.arguments objectAtIndex:0]]];
+    if(result != EPOS_OC_SUCCESS){
+        plug = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"Could not set text font"];
+    } else {
+        plug = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
+    }
+    [self.commandDelegate sendPluginResult:plug callbackId:[command callbackId]];
+}
+
+//(int)type;
+- (void) addCut:(CDVInvokedUrlCommand *)command {
+    CDVPluginResult* plug;
+    int result = [builder addCut:[self getBuilderType:(int)[command.arguments objectAtIndex:0]]];
+    if(result != EPOS_OC_SUCCESS){
+        plug = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"Could not cut"];
+    } else {
+        plug = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
+    }
+    [self.commandDelegate sendPluginResult:plug callbackId:[command callbackId]];
+}
+
+- (void) sendToPrinter:(CDVInvokedUrlCommand *)command {
+    CDVPluginResult* plug;
+    unsigned long status = 0;
+    unsigned long battery = 0;
+    int result = [printer sendData:builder Timeout:SEND_TIMEOUT Status:&status Battery:&battery];
+    if(result != EPOS_OC_SUCCESS){
+        plug = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"Could not print"];
+    } else {
+        plug = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
+    }
+    [self.commandDelegate sendPluginResult:plug callbackId:[command callbackId]];
+    
 }
 
 @end
